6f4f17503a375254483761a41f6fabe42d091a47
MATHEUS BRITTO <202407139019@alunos.estacio.br>
2024-07-28 22:35:56 -0300

    Adicionar arquivos de projeto.

---
+++ LarguraDeTriangulo/Program.cs
@@ -0,0 +1,27 @@
+﻿// Fazer um programa para ler os valores da largura e altura  de um retângulo. Em seguida,
+// mostrar na tela o valor de sua área, perímetro e diagonal. Usar uma classe como mostrado
+// no projeto ao lado.
+
+using Course;
+using System;
+using System.Globalization;
+
+namespace LarguraDeTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x;
+            x = new Triangulo();
+            Console.WriteLine("Entre com o lado A");
+            x.A = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o lado B");
+            x.B = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o lado C");
+            x.C = Double.Parse(Console.ReadLine());
+            Console.WriteLine(x);
+
+        }
+    }
+}
---
+++ LarguraDeTriangulo/Triangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LarguraDe
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+        public double Area()
+        {
+            double P = (A + B + C) / 2.0;
+            return Math.Sqrt(P * (P - A) * (P - B) * (P - C));
+        }
+        public double Perimetro()
+        {
+            double p = (A + B + C);
+            return (p);
+        }
+        public override string ToString()
+        {
+            return "O valor da area do triangulo é de: "
+                + Area
+                +"\nO valor do perimetro do triangulo é de"
+                + Perimetro;
+                
+        }
+    }
+}
